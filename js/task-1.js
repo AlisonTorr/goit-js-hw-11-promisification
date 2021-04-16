9d07821e70b21acf7cd285153da53c7054d15643
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-04-16 19:33:58 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,5 @@
 const delay = (ms) => {
   const promise = new Promise((resolve) => {
-    const logger = (time) => console.log(`Resolved after ${time} ms`);
     setTimeout(() => {
       resolve(logger(ms));
     }, ms);
@@ -8,6 +7,8 @@ const delay = (ms) => {
 
   return promise;
 };
+
+const logger = (time) => console.log(`Resolved after ${time} ms`);
 
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
